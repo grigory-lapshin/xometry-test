d43f63495c9c65f573773d3e5d23f40e9d648d6d
Grigoriy Lapshin <g.lapshin@rocketguys.com>
2020-04-08 12:37:11 +0300

    fix: missed error labels

--- src/components/InputError.js
+++ src/components/InputError.js
@@ -1,6 +1,12 @@
+// @flow
 import React from 'react';
 
-export default function (meta) {
+declare type Props = {
+  meta: Object,
+};
+
+export default function (props: Props) {
+  const { meta } = props;
   if (meta.touched && meta.error)
     return <div className="text-red-600">{meta.error}</div>;
   return null;
